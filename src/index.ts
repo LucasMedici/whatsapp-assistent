aed8a9c3a553ae5bd8b07e2ccbc3b4410c96fa11
LucasMedici <lucasrpmedici@gmail.com>
2025-10-30 20:27:03 -0300

    feat: base do webhook e requisicoes do insomnia importadas em yml

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import webhookRoutes from './routes/webhook';
 
 const app = express();
 dotenv.config();
@@ -9,6 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/serverstatus', (req, res) => {
   res.send('on');
 });
+
+app.use('/webhook', webhookRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,1 +1,18 @@
-import { Router } from 'express';
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.post("/messages", async (req: Request, res: Response) =>  {
+    const { Body, From } = req.body;
+    
+    console.log("Mensagem recebida")
+    console.log("De: ", From);
+    console.log("Conteúdo: ", Body);
+
+    // processar mensagem
+    // salvar no banco
+
+    return res.status(200).send("Mensagem recebida com sucesso");
+});
+
+export default router;
